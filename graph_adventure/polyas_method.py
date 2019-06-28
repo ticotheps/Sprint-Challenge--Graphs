7ad7b9ac3d238431f0e3cd8a1c854af9903c9de5
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-28 12:23:41 -0400

    Creates a 'polyas_method.py' file to plan out problem-solving approach

---
+++ graph_adventure/polyas_method.py
@@ -0,0 +1,19 @@
+#------------------------UNDERSTANDING THE PROBLEM-----------------------
+
+#  Problems: 
+#     (1) The list of [traversal] directions provided is NOT complete.
+#     (2) There is no current graph that represents all 500 rooms in the
+#         game.
+#     (3) All the tests in adv.py DO NOT pass when the code is executed.
+    
+#  Provided Specs:
+#     (1) This is an adventure game.
+#     (2) The game has a total of 500 rooms that can be visited by a player.
+#     (3) The game only has ONE player (you).
+#     (4) Through the list of directions in 'traversalPath,' you can
+#         perform ONE move at a time, and each move will be made in ONE of 
+#         the following directions: 
+#            (a) n (North)
+#            (b) s (South) 
+#            (c) e (East) 
+#            (d) w (West) 
