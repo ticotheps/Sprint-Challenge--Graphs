ce910f1ea78652aa5107ee582894ea785029330f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-28 12:30:35 -0400

    Defines what the player's objectives are in the 'polyas_method.py' file

--- graph_adventure/polyas_method.py
+++ graph_adventure/polyas_method.py
@@ -17,3 +17,25 @@
 #            (b) s (South) 
 #            (c) e (East) 
 #            (d) w (West) 
+
+#  Player's Objective: 
+#     (1) Visit all 500 rooms in the SHORTEST number of moves possible.
+#     (2) Construct a dictionary that represents a graph of all 500 
+#         rooms, filling the dictionary one entry at a time, AS the player 
+#         is traversing through all 500 rooms.
+#            (a) i.e. - {
+#                         0: {'n': '?', 's': 5, 'w': '?', 'e': '?'},
+#                         5: {'n': 0, 's': '?', 'e': '?'}
+#                       }
+#     (3) Create a list of directions called 'traversalPath' that 
+#         stores [the directions of] each move you make while visiting
+#         all 500 rooms.
+#            (a) The moves will be stored in sequential order with index 0
+#                being the very FIRST move that is made from the 
+#                'startingRoom'.
+#                   (i) i.e. - traversalPath = ['n', 'e']
+#     (4) You will know when you all 500 rooms have been visited if the
+#         following TWO conditions have been met:
+#            (a) There are a total of 500 entries in your adjacency 
+#                dictionary.
+#            (b) There are no more "?" values in the adjacency dictionary.
